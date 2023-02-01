2a2cfa357754abf4375923c799718884e7e29c11
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-02-01 20:37:32 +0600

    js string case

---
+++ case.js
@@ -0,0 +1,16 @@
+const userName = "blackPinK";
+const inputName = "blacKpinK";
+
+if (userName.toLowerCase === inputName.toLowerCase) {
+   console.log("valid user");
+}
+else{
+   console.log("invalid user")
+}
+if (userName.toUpperCase === inputName.toUpperCase) {
+   console.log("valid user");
+}
+else{
+   console.log("invalid user")
+}
+
